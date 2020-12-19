e14f34c4a34d565dbb60940dfdf20809bad4d776
John Edmiston <johnkedmiston@gmail.com>
2020-12-19 15:36:15 -0800

    set up for package

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="latex-ds",
+      version='1.0',
+      description="LaTeX for reporting data",
+      packages=["latex_reports"])
